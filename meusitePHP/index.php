a5f2b6dc5c8d45344cc249f32d27258d0fdb264d
NikOlausReis <117763456+NikOlausReis@users.noreply.github.com>
2023-08-01 14:41:52 -0300

    Add files via upload

---
+++ meusitePHP/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 5 /* + 15 */ + 5;
+echo "<br>",$x;
+// o comando echo imprime na tela
+echo "My first PHP script!";
+$turma2 = "Vamos desenvolver website em PHP";
+echo "<br>",$turma2;
+
+$txt = "Hello world!";
+$z = 5;
+$y = 10.5;
+echo "<br>",$txt, " x = ",$z, " y = ",$y
+
+?>
+
+</body>
+</html>
